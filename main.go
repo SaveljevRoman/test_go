93700e0beaa2e9a3ba34813f5497cf1527d74879
roman <saveljev.roman@gmali.com>
2023-09-29 20:24:42 +0300

    3.6.1

--- main.go
+++ main.go
@@ -1,66 +1,40 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
-	"math/rand"
-	"time"
 )
-
-// выполняет какую-то операцию,
-// обычно быстро, но иногда медленно
-func work() int {
-	if rand.Intn(10) < 8 {
-		time.Sleep(10 * time.Millisecond)
-	} else {
-		time.Sleep(200 * time.Millisecond)
-	}
-	return 42
-}
-
-// выполняет функцию fn() c таймаутом timeout и возвращает результат
-// если в течение timeout функция не вернула ответ - возвращает ошибку
-func withTimeout(fn func() int, timeout time.Duration) (int, error) {
-	var result int
-
-	done := make(chan struct{})
-	go func() {
-		result = fn()
-		close(done)
-	}()
-
-	select {
-	case <-done:
-		return result, nil
-	case <-after(timeout):
-		return 0, errors.New("timeout")
-	}
-}
 
 // начало решения
 
-// возвращает канал, в котором появится значение
-// через промежуток времени dur
-func after(dur time.Duration) <-chan time.Time {
-	ch := make(chan time.Time, 1)
-
+// генерит целые числа от start и до бесконечности
+func generate(ctx context.Context, start int) <-chan int {
+	out := make(chan int)
 	go func() {
-		time.Sleep(dur)
-		ch <- time.Now()
+		defer close(out)
+		for i := start; ; i++ {
+			select {
+			case out <- i:
+			case <-ctx.Done():
+				return
+			}
+		}
 	}()
-	return ch
+	return out
 }
 
 // конец решения
 
 func main() {
-	for i := 0; i < 10; i++ {
-		start := time.Now()
-		timeout := 50 * time.Millisecond
-		if answer, err := withTimeout(work, timeout); err != nil {
-			fmt.Printf("Took %v. Error: %v\n", time.Since(start), err)
-		} else {
-			fmt.Printf("Took %v. Result: %v\n", time.Since(start), answer)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	generated := generate(ctx, 11)
+	for num := range generated {
+		fmt.Print(num, " ")
+		if num > 14 {
+			break
 		}
 	}
+	fmt.Println()
 }
